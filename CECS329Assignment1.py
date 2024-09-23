1a0780d5478a05fa4b6b3f8fd4bacd3ee2bc7c00
Eden <ecoding@pm.me>
2024-09-22 18:54:55 -0700

    Added Angelo's name

--- CECS329Assignment1.py
+++ CECS329Assignment1.py
@@ -1,4 +1,4 @@
-#Names: Eden Doss-Fillmore, Noah Daniels, 
+#Names: Eden Doss-Fillmore, Noah Daniels, Angelo Cervana
 #Tools used: Visual Studio Code, Discord, GitHub, Python, ChatGPT
 #GitHub Link: https://github.com/ecodingg/329-Project1
 
